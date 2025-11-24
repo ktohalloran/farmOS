e250faceae854c84681e34517445cde0cc5bd384
Katie Brown <kbrown@pointblue.org>
2025-11-24 09:36:03 -0500

    Add form protection library to prevent navigation with unsaved changes

---
+++ modules/core/form/js/form_protection.js
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * Show warning when a user is about to navigate away from an unsaved, dirty form
+ */
+(function ($, Drupal) {
+
+    "use strict"
+
+    Drupal.behaviors.form_protection = {
+        initialFormState: {},
+
+        attach: function (context) {
+            let behavior = Drupal.behaviors.form_protection;
+            let allowClick = false;
+            let formIsDirty = false;
+
+            // get initial form state on initial load
+            $("form", context).each(function () {
+                behavior.initialFormState[$(this).attr("id")] = $(this).serialize();
+            });
+
+            // Let all form submit buttons through.
+            $("input[type='submit'], button[type='submit']").each(function() {
+                $(this).addClass("allow-submit");
+                $(this).click(function() {
+                    allowClick = true;
+                });
+            });
+
+            // Catch all links and buttons EXCEPT for "#" links.
+            $("a, button, input[type='submit']:not(.allow-submit), button[type='submit']:not(.allow-submit)")
+                .each(function() {
+                    $(this).click(function() {
+                        formIsDirty = behavior.checkForUnsavedChanges()
+                        // If the user clicked on a # link or the form is not dirty, let click through; otherwise,
+                        // return something other than undefined to trigger onbeforeunload
+                        if (formIsDirty && $(this).attr("href") !== "#") {
+                            return 0;
+                        }
+                    });
+                });
+
+            // Handle backbutton, exit etc.
+            window.onbeforeunload = function () {
+                if (formIsDirty && !allowClick) {
+                    allowClick = false;
+                    return (Drupal.t("You have unsaved changes."))
+                }
+            }
+        },
+
+        checkForUnsavedChanges: function () {
+            let behavior = Drupal.behaviors.form_protection;
+            let formIsDirty = false
+            $("form").each(function () {
+                const updatedFormState = $(this).serialize();
+                formIsDirty = updatedFormState !== behavior.initialFormState[$(this).attr("id")]
+            })
+            return formIsDirty
+        }
+    }
+})(jQuery, Drupal)
