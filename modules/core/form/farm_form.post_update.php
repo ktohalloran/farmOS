f57be5e748da1b8d1df956727d2ec26528959743
Katie Brown <kbrown@pointblue.org>
2025-12-08 16:34:47 -0500

    Add config schema and post update hook

---
+++ modules/core/form/farm_form.post_update.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Post update hooks for the farmOS Form module.
+ */
+
+declare(strict_types=1);
+
+/**
+ * Add new form protection configuration.
+ */
+function farm_form_post_update_add_form_protection_config() {
+  $config_factory = \Drupal::configFactory();
+  $config = $config_factory->getEditable('farm_form.settings');
+
+  // Set new configuration values.
+  $config->set('enable_form_protection', true);
+
+  // Save the configuration.
+  $config->save();
+}
