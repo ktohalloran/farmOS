96bf2d7ce4bfe316a86a67e48b877a7db03fb572
ktohalloran <77936689+ktohalloran@users.noreply.github.com>
2025-12-15 11:45:06 -0500

    JS rewrite
    Co-authored-by: Symbioquine <30754460+symbioquine@users.noreply.github.com>

--- modules/core/form/js/form_protection.js
+++ modules/core/form/js/form_protection.js
@@ -2,75 +2,61 @@
  * @file
  * Show warning when a user is about to navigate away from an unsaved, dirty form.
  */
+/**
+ * @file
+ * Show warning when a user is about to navigate away from an unsaved, dirty form.
+ */
 (function ($, Drupal) {
 
-    "use strict";
+    "use strict"
+
+    const initialFormState = {};
+
+    // Track whether submit was clicked "globally" over all attached DOM so we can ignore those
+    // causes of window.onbeforeunload firing and not prevent form submission
+    let submitWasClicked = false;
+
+    const doesAnyProtectedFormContainChanges = function() {
+        const anyFormElementChanged = $("form.protected :input").is(function(i, el) {
+            if (!el.id or !Object.hasOwn(initialFormState, el.id)) {
+                return false;
+            }
+
+            const formElementChanged = $(this).serialize() !== initialFormState[elId];
+
+            return formElementChanged;
+        });
+
+        return anyFormElementChanged;
+    };
 
     Drupal.behaviors.form_protection = {
 
-        initialFormState: {},
-
         attach: function (context) {
-            const behavior = Drupal.behaviors.form_protection;
-            let allowClick = false;
-            let formIsDirty = false;
-
-            // Get initial form state on initial load.
-            $("form.entity-or-quick-form-protected :input", context).each(function () {
-                behavior.initialFormState[$(this).attr("id")] = $(this).serialize();
+            // Save the initial form state of any input elements in the attached DOM
+            $("form.protected :input", context).each(function () {
+                if (this.id) {
+                  initialFormState[this.id] = $(this).serialize();
+                }
             });
 
             // Let all form submit buttons through.
-            $("input[type='submit'], button[type='submit']").each(function() {
-                $(this).addClass("allow-submit");
-                $(this).click(function() {
-                    allowClick = true;
-                });
+            $("input[type='submit'], button[type='submit']", context).click(function() {
+                submitWasClicked = true;
             });
 
-            // Catch all links and buttons EXCEPT for "#" links.
-            $("a, button, input[type='submit']:not(.allow-submit), button[type='submit']:not(.allow-submit)")
-                .each(function() {
-                    $(this).click(function() {
-                        formIsDirty = behavior.checkForUnsavedChanges();
-                        // If the user clicked on a # link or the form is not dirty, let click through; otherwise,
-                        // return something other than undefined to trigger onbeforeunload.
-                        if (formIsDirty && $(this).attr("href") !== "#") {
-                            return 0;
-                        }
-                    });
-                });
-
-            // Handle backbutton, exit etc.
+            // Handle navigation, backbutton, exit etc.
             window.onbeforeunload = function () {
-                if (formIsDirty && !allowClick) {
-                    allowClick = false;
-                    return (Drupal.t("You have unsaved changes."));
+                // If the form contains changes and this unload event was not caused by form submission
+                // cause the browser to prompt the user whether they want to leave the page.
+                if (doesAnyProtectedFormContainChanges() && !submitWasClicked) {
+                    // For very old browsers show custom text (modern browsers do not allow customizing the text).
+                    return Drupal.t("You have unsaved changes.");
                 }
+
+                // No return here since that is the preferred way to not prompt
             }
-        },
+        }
 
-        checkForUnsavedChanges: function () {
-            const behavior = Drupal.behaviors.form_protection;
-            let formIsDirty = false;
-
-            $("form.entity-or-quick-form-protected :input").each(function () {
-                const elId = $(this).attr("id");
-
-                // Check new state against initial state if available; if it's not, move on.
-                if (elId && Object.keys(behavior.initialFormState).includes(elId)) {
-                    const updatedFormState = $(this).serialize();
-                    formIsDirty = updatedFormState !== behavior.initialFormState[elId];
-                }
-
-                // If we find a value that has been changed, break out of the loop (which in jQuery apparently
-                // means return false).
-                if (formIsDirty) {
-                    return false;
-                }
-            })
-            return formIsDirty;
-        },
-
-    }
-})(jQuery, Drupal)
+    };
+})(jQuery, Drupal);
